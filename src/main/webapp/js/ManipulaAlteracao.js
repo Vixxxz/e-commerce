bd64c3301c11cbc8077f00cc81a060044dc7d88d
Vixxxz <ryan.takata@outlook.com>
2025-03-06 15:04:53 -0300

    Ajusta Alterar

--- src/main/webapp/js/ManipulaAlteracao.js
+++ src/main/webapp/js/ManipulaAlteracao.js
@@ -60,7 +60,7 @@ function preencherFormulario(clienteArray) {
 function formatarDataParaInput(data) {
     if (!data) return "";
     const dataObj = new Date(data);
-    return dataObj.toISOString().split("T")[0]; // Retorna "YYYY-MM-DD"
+    return dataObj.toISOString().split("T")[0];
 }
 
 function preencherRadioButton(name, valor) {
@@ -106,7 +106,7 @@ function montaJson(id) {
     return JSON.stringify({
         Cliente: {
             id: id,
-            ranking: clienteRanking, // Usa o ranking armazenado
+            ranking: clienteRanking,
             nome: document.getElementById("nome-altera").value,
             genero: document.querySelector('input[name="genero-altera"]:checked').value,
             cpf: document.getElementById("cpf-altera").value,
